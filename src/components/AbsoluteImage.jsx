75314c640c31bf9344d12148bfc0a0dc8a11fbe7
Leons Aleksandrovs <skrazzo@proton.me>
2024-02-07 16:49:15 +0200

    section 1 pc view finished

---
+++ src/components/AbsoluteImage.jsx
@@ -0,0 +1,38 @@
+
+export default function AbsoluteImage({ 
+            zIndex = 50,
+            width = '75px',
+            height = '75px' ,
+            className = '',
+            top = 'auto',
+            right = 'auto',
+            bottom = 'auto',
+            left = 'auto',
+            src = '',
+            rotate = '0deg',
+            style = {}
+        },
+        props
+    ) {
+    
+    return (
+        <img 
+            className={` ${className}`}
+            style={{
+                transform: `rotate(${rotate}deg)`,
+                zIndex: zIndex,
+                position: 'absolute',
+                top: top,
+                right: right,
+                bottom: bottom,
+                left: left,
+                width: width,
+                height: height,
+                ...style,
+                
+            }}
+            {...props}
+            src={src}
+        />
+    )
+}
--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -1,11 +1,53 @@
 import s from '../scss/FirstSection.module.scss';
+import AbsoluteImage from './AbsoluteImage';
+import img1 from '../assets/icons/coffee.png';
+import img2 from '../assets/icons/coffeeBean.png';
+import img3 from '../assets/icons/coffeePot.png';
+import wave from '../assets/waves/wave1.png';
 
 export default function FirstSection() {
     return (
-        <div className='p-20 '>
-            <p className={`${s.font}`}>Coffee Time</p>
-            <p className={`${s.headline}`}>your headline</p>
-            <p className={`${s.paragraph}`}>Ullamco veniam proident aute enim. Consequat culpa qui elit Lorem laboris anim laboris sint voluptate. Consequat sint dolore mollit eiusmod laborum incididunt. Anim officia laborum exercitation eu.</p>
+        <div className='relative'>
+            <p className={`ml-20 mt-20 ${s.font}`}>Coffee Time</p>
+            <p className={`ml-20 mt-8 ${s.headline}`}>your headline</p>
+            <p className={`ml-20 mt-4 ${s.paragraph}`}>Ullamco veniam proident aute enim. Consequat culpa qui elit Lorem laboris anim laboris sint voluptate. Consequat sint dolore mollit eiusmod laborum incididunt. Anim officia laborum exercitation eu.</p>
+            <button className={`ml-20 mt-8 ${s.btn}`}>Order Now</button>
+
+            <div className={`${s.coffee}`}></div>
+
+            <AbsoluteImage
+                className='opacity-30'
+                rotate='30'
+                right='700px'
+                top='0'
+                src={img1}
+            />
+
+            <AbsoluteImage
+                className='opacity-30'
+                rotate='50'
+                right='850px'
+                top='210px'
+                src={img2}
+            />
+
+            <AbsoluteImage
+                className='opacity-30'
+                style={{
+                    transform: 'scaleX(-1) scale(1.5) rotate(50deg)',
+                    
+                }}
+                right='1350px'
+                bottom='0'
+                src={img3}
+            />
+
+            <AbsoluteImage 
+                width='100%'
+                height='auto'
+                bottom='-300px'
+                src={wave}
+            />
         </div>
     )
 }
